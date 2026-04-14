406058556403a8029946f635a36d38e53458c734
Cyprien Cres <140599994+Cyprien-Cres@users.noreply.github.com>
2026-04-15 00:42:19 +0200

    P6_1 - Initialisation du projet avec create_react_router

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Login } from "~/login/login";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Sportsee - Login" },
+    { name: "login", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Login />;
 }
